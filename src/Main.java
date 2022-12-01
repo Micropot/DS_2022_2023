52c894e9d2f51588ee301612abe2af79ee7f2a54
Arthur Lamard <arthurlamard@MacBook-Pro-de-Arthur-2.local>
2022-12-01 07:51:47 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+// DS JAVA 2022/2023
+//********** Arthur Lamard et COline Michaud**********
+//M1-G3
+//01/12/2022
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
